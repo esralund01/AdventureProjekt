17a5d5c415cd1cd5dc95c28644d2df3f592f1db1
esralund01 <calu0001.stud.kea.dk>
2024-03-04 09:53:30 +0100

    commit med Hello World

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("TTTTTT teeeeeest");
+        System.out.println("hej");
     }
 }
