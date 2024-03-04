b7775825945fbaa80264f943e4d501418af46e2f
NikolajWild <nikolajmeier@Nikolajs-MacBook-Pro.local>
2024-03-04 09:55:05 +0100

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
         System.out.println("Hello world!");
         System.out.println("TTTTTT teeeeeest");
         System.out.println("heyhey");
-
+        System.out.println("admsadsnaod");
     }
 }
