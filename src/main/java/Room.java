193902d7c479d3b8a277b17a9a88299817a84cfd
esralund01 <calu0001.stud.kea.dk>
2024-03-05 13:17:29 +0100

    commit

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,32 @@
 public class Room {
+    //attributter
+    private String name;
+    private String description;
+private Room north;
+private Room south;
+private Room west;
+private Room east;
+
+    //constructor
+public Room (String name, String description) {
+    this.name = name;
+    this.description = description;
+}
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
 }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,15 +1,3 @@
-public class UserInterfce {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("TTTTTT teeeeeest");
-        System.out.println("hej");
-        System.out.println("hestw2");
-        System.out.println("heyhey");
-        System.out.println("admsadsnaod");
-        System.out.println("djdjdjd");
-        System.out.println("jj");
-        System.out.println("rgrs");
-        System.out.println("fatter hat");
-        System.out.println("fatter hat2");
-    }
+public class UserInterface {
+
 }
