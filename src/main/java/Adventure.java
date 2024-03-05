df647ec7f5124ebc6158f7b9c85d0e2836b05628
esralund01 <calu0001.stud.kea.dk>
2024-03-05 15:02:30 +0100

    commit 1

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -70,4 +70,5 @@ public class Adventure {
     public String look() {
         return currentRoom.getDescription();
     }
+
 }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -2,24 +2,76 @@ import java.util.Scanner;
 
 public class UserInterface {
 
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner;
+    private Adventure adventure;
 
-    public void startProgram(){
-        String look = "look";
-        String start = "start";
-        String exit = "exit";
-        String help = "help";
-
-        System.out.println("Welcome to Adventure game, type start to begin the game");
-
-        while (start != exit){
-
-
-
-
-        }
-
-
+    public UserInterface() {
+        scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        adventure = new Adventure();
     }
 
-}
+    public void startProgram() {
+
+        System.out.println("Welcome to Adventure game, you have begun the game. ");
+
+        boolean gameIsRunning = true;
+
+        while (gameIsRunning) {
+            System.out.println("which direction do you want to go?");
+            String input = scanner.next();
+
+            if (input.equals("go north")) {
+                System.out.println("going north." + adventure.look());
+                adventure.goNorth();
+
+            } else if (input.equals("go west")) {
+                if (adventure.goWest()){
+                    System.out.println("going west. " + adventure.look());
+            } else {
+                    System.out.println("you cannot go that way");
+        }
+
+            } else if (input.equals("go south")) {
+                if (adventure.goSouth()){
+                    System.out.println("going south. " + adventure.look());
+                } else {
+                    System.out.println("you cannot go that way");
+                }
+
+
+            } else if (input.equals("go east")) {
+                if (adventure.goEast()){
+                    System.out.println("going east. " + adventure.look());
+                } else {
+                    System.out.println("you cannot go that way");
+                }
+
+            } else if (input.equals("look")) {
+                System.out.println("looking around. you are in room: " + adventure.look());
+
+             } else if(input.equals("exit")) {
+                gameIsRunning = false;
+                System.out.println("the game has ended.");
+
+            } else if (input.equals("help")){
+                helpProgram();
+
+            } else {
+                System.out.println("this input doesnt work. type 'help' to get help");
+
+            }
+        }
+    }
+
+    public void helpProgram(){
+            System.out.println("help is on its way.");
+        System.out.println("typing 'go north' will make you go north");
+        System.out.println("typing 'go west' will make you go west");
+        System.out.println("typing 'go east' will make you go east");
+        System.out.println("typing 'go south' will make you go south");
+        System.out.println("typing 'exit' will make you exit the game");
+        System.out.println("typing 'look' will give you a description of the room you are in");
+        }
+    }
+
