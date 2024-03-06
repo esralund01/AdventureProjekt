fc90a8f7a0132b13f9c797a8e57159f0d737b14f
esralund01 <calu0001.stud.kea.dk>
2024-03-06 09:07:32 +0100

    player klasse tilføjet

---
+++ src/main/java/Player.java
@@ -0,0 +1,51 @@
+public class Player {
+
+    private Room currentRoom;
+
+
+
+    public Player(Room firstRoom) {
+        firstRoom = currentRoom;
+    }
+
+    public boolean goNorth() {
+        if (currentRoom.getNorth() == null) {
+            return false;
+        } else {
+            currentRoom = currentRoom.getNorth();
+            return true;
+        }
+    }
+
+    public boolean goWest() {
+        if (currentRoom.getWest() == null) {
+            return false;
+        } else {
+            currentRoom = currentRoom.getWest();
+            return true;
+        }
+    }
+
+    public boolean goEast() {
+        if (currentRoom.getEast() == null) {
+            return false;
+        } else {
+            currentRoom = currentRoom.getEast();
+            return true;
+        }
+    }
+
+    public boolean goSouth() {
+        if (currentRoom.getSouth() == null) {
+            return false;
+        } else {
+            currentRoom = currentRoom.getSouth();
+            return true;
+        }
+    }
+
+    public String look() {
+        return currentRoom.getDescription();
+    }
+
+}
